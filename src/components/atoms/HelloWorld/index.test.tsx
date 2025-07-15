b2b4ce544f2a1c50fb533bb4d9f3d3ecb5f47863
Vee Uye <vee.uye@gmail.com>
2025-07-15 22:20:52 +0100

    chore: restruture according to atomic design principles

--- src/components/atoms/HelloWorld/index.test.tsx
+++ src/components/atoms/HelloWorld/index.test.tsx
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
-import HelloWorld from './HelloWorld'
+import HelloWorld from './index.tsx'
 
 describe('HelloWorld', () => {
   it('should render the component without crashing', () => {
