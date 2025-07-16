e6aa9a43d5f7ba66e0258c0161b96bcfe3e0afd1
Vee Uye <vee.uye@gmail.com>
2025-07-16 07:31:56 +0100

    chore: update configs and install astro tailwind and astro react

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,45 @@
+// .eslintrc.cjs
+/* eslint-env node */
+
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-        node: true,
+  root: true,
+  // Base environment for the project
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  // Start with the recommended presets
+  extends: [
+    'eslint:recommended',
+    'plugin:astro/recommended',
+    'plugin:astro/jsx-a11y-recommended',
+    'prettier', // This must be last
+  ],
+  // Main parser for TypeScript files
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+  },
+  overrides: [
+    {
+      // Configuration for your Astro components
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+      },
+      // Extend TypeScript rules for the script part of Astro files
+      extends: ['plugin:@typescript-eslint/recommended'],
     },
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:astro/recommended',
-        'plugin:jsx-a11y/recommended',
-        'prettier',
-    ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module',
+    {
+      // This is the key fix for your astro.config.mjs file
+      files: ['astro.config.mjs'],
+      // We are only turning off the "not defined" rule for this one specific file
+      rules: {
+        'no-undef': 'off',
+      },
     },
-    plugins: ['@typescript-eslint'],
-    rules: {
-    },
-};
+  ],
+}
