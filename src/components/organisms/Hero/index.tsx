8327ba51b37db2fc1d2025fdeb00eeec7c3e33bf
Vee Uye <vee.uye@gmail.com>
2025-08-21 22:20:08 +0100

    style: hero updates

--- src/components/organisms/Hero/index.tsx
+++ src/components/organisms/Hero/index.tsx
@@ -13,33 +13,32 @@ interface HeroProps {
 
 const Hero = ({ title, subtitle, buttonText, buttonLink }: HeroProps) => {
   return (
-    <>
-      <section className={styles.base}>
-        <div className={styles.inner}>
-          <div className={styles.heroText}>
-            <h1 className={styles.heroTitle}>{title}</h1>
-            <p className={styles.heroSubtitle}>{subtitle}</p>
-            <a href={buttonLink}>
-              <Button size="3" className={styles.heroButton}>
-                {buttonText}
-                <ArrowRightIcon />
-              </Button>
-            </a>
-          </div>
-          <div className={styles.heroImageWrapper}>
-            <img
-              src={heroImage.src}
-              alt="Children and teacher with hands raised"
-              className={styles.heroImage}
-              loading="lazy"
-              aria-hidden="true"
-            />
-          </div>
+    <section className={styles.base}>
+      <div className={styles.heroImageWrapper}>
+        <img
+          src={heroImage.src}
+          alt="Children and teacher with hands raised"
+          className={styles.heroImage}
+          loading="lazy"
+          aria-hidden="true"
+        />
+      </div>
 
-          <div className={styles.heroOverlay}></div>
+      <div className={styles.heroOverlay}></div>
+
+      <div className={styles.inner}>
+        <div className={styles.heroText}>
+          <h1 className={styles.heroTitle}>{title}</h1>
+          <p className={styles.heroSubtitle}></p>
+          <a href={buttonLink}>
+            <Button size="3" className={styles.heroButton}>
+              {buttonText}
+              <ArrowRightIcon />
+            </Button>
+          </a>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   )
 }
 
